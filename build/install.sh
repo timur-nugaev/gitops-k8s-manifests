4154d62000d655b3bd8e6a377fba125db4d827a4
Timur Nugaev <nugaev.t@gmail.com>
2023-10-23 10:02:48 +0300

    update

--- build/install.sh
+++ build/install.sh
@@ -4,4 +4,6 @@
 # kubectl apply -n build -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/git-cli/0.4/git-cli.yaml
 # kubectl apply -n build -f https://api.hub.tekton.dev/v1/resource/tekton/task/kubernetes-actions/0.2/
 
-kubectl apply -f build-rbac.yaml
+# kubectl apply -f build-rbac.yaml
+
+# kubectl apply -n build -f https://api.hub.tekton.dev/v1/resource/tekton/task/argocd-task-sync-and-wait/0.2/raw

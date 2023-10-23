2f3569ccc48fefe8831c159fa090df9419843fb1
Timur Nugaev <nugaev.t@gmail.com>
2023-10-23 11:21:46 +0300

    branch creation script was added

---
+++ 1_create_branch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+NEW_BRANCH_NAME=$1
+BASE_CONFIGURATION=$2
+SOURCE_BRANCH_NAME=$3
+
+
+if [ -z "$NEW_BRANCH_NAME" ]
+then
+    echo "new branch name is required"
+    exit 1
+fi
+
+if [ -z "$BASE_CONFIGURATION" ]
+then
+    BASE_CONFIGURATION=dev
+fi
+
+if [ -z "$SOURCE_BRANCH_NAME" ]
+then
+    SOURCE_BRANCH_NAME=dev
+fi
+
+sed -e "s/NEW_BRANCH_NAME/$NEW_BRANCH_NAME/" \
+  -e "s|BASE_CONFIGURATION|$BASE_CONFIGURATION|g" \
+  -e "s|SOURCE_BRANCH_NAME|$SOURCE_BRANCH_NAME|g" ./build/new-branch-pipeline-run.template \
+  | kubectl create -f -  
+
+

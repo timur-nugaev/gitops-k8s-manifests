8f90c9b01e1b7ccf37c453a4186815c2acfc83e2
Timur Nugaev <nugaev.t@gmail.com>
2023-10-25 13:27:35 +0300

    cleanup

--- build/install.sh
+++
@@ -1,9 +0,0 @@
-# kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
-# kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/interceptors.yaml
-
-# kubectl apply -n build -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/git-cli/0.4/git-cli.yaml
-# kubectl apply -n build -f https://api.hub.tekton.dev/v1/resource/tekton/task/kubernetes-actions/0.2/
-
-# kubectl apply -f build-rbac.yaml
-
-# kubectl apply -n build -f https://api.hub.tekton.dev/v1/resource/tekton/task/argocd-task-sync-and-wait/0.2/raw
